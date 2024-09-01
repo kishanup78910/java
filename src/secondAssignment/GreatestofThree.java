11fe366deed02a36a11d845147261ed4a2e2b80b
kishanup78910 <kishanup789@gmail.com>
2024-09-01 23:24:30 +0530

    Greatest_month_palindrom

---
+++ src/secondAssignment/GreatestofThree.java
@@ -0,0 +1,20 @@
+package secondAssignment;
+
+import java.util.Scanner;
+
+public class GreatestofThree {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+
+        if(a>b && a>c){
+            System.out.println("A is greater");
+        }else if(b>c && b>a){
+            System.out.println("B is greater");
+        }else{
+            System.out.println("C is greater");
+        }
+    }
+}
---
+++ src/secondAssignment/palindrom.java
@@ -0,0 +1,10 @@
+package secondAssignment;
+
+import java.util.Scanner;
+
+public class palindrom {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int a=sc.nextInt();
+    }
+}
